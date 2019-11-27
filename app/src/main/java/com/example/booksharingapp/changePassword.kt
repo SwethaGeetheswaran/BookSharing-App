2b734dca2745309ec0c5e5d5c3abc7c29735046f
swethaGeetheswaran <sgeet1@unh.newhaven.edu>
2019-11-27 15:33:05 -0500

    Edit Profile - Part 2

---
+++ app/src/main/java/com/example/booksharingapp/changePassword.kt
@@ -0,0 +1,155 @@
+package com.example.booksharingapp
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_edit_profile.*
+import kotlinx.android.synthetic.main.change_password.*
+import java.util.HashMap
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class changePassword : AppCompatActivity() {
+
+    private val TAG = "changePassword"
+    private var userPassword: String? = null
+    private lateinit var mDatabase: FirebaseDatabase
+    private lateinit var currentUserID:String
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var getCurrentPasswordInput:String
+    private lateinit var mUsersDbRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.change_password)
+        supportActionBar?.setTitle(R.string.change_password)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance()
+        userPassword = intent.getStringExtra("password").toString()
+        currentUserID = mAuth.currentUser!!.uid
+        mUsersDbRef = mDatabase.reference.child("Users").child(currentUserID)
+
+
+        save_password_button.setOnClickListener {
+            getCurrentPasswordInput = current_password_id.text.toString()
+            Log.v(TAG, "currentPass:" +getCurrentPasswordInput)
+            if(TextUtils.isEmpty(getCurrentPasswordInput)){
+                  Toast.makeText(this,"Please enter your current password",Toast.LENGTH_SHORT).show()
+            }else if(! userPassword.equals(getCurrentPasswordInput)) {
+                Toast.makeText(this, "Mis-match between your current passwords!. Please re-try again.", Toast.LENGTH_SHORT).show()
+            }else{
+                    val newPassword = new_password_id.text.toString()
+                    Log.v(TAG, "newPass:" +newPassword)
+                    val confirmNewPassword = new_confirm_password.text.toString()
+                    Log.v(TAG,"confirmPass:" +confirmNewPassword)
+                    if(!TextUtils.isEmpty(newPassword) || !TextUtils.isEmpty(confirmNewPassword)) {
+                        if ((newPassword.length < 8 || confirmNewPassword.length < 8) &&
+                            (!isValidPassword(newPassword) || !isValidPassword(confirmNewPassword))
+                        ) {
+                            Toast.makeText(
+                                this,
+                                "Password length is too short. Please enter a minimum of 8 characters.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else if (newPassword.equals(confirmNewPassword)) {
+                            re_authenticate()
+                            updateFirebaseDatabase()
+                        }
+                    }
+
+            }
+
+        }
+
+        cancel_password_button.setOnClickListener {
+            startActivity(editProfileActivity.getLaunchIntent(this))
+        }
+
+    }
+
+    private fun updateFirebaseDatabase() {
+        val userinfo_updates_hashMap = HashMap<String,Any>()
+        userinfo_updates_hashMap.put("password",new_confirm_password.text.toString())
+
+        mUsersDbRef.updateChildren(userinfo_updates_hashMap)
+            .addOnSuccessListener {
+                Log.v(TAG,"Updated in Firebase database successfully")
+            }
+            .addOnFailureListener {
+                Log.v(TAG, it.printStackTrace().toString())
+            }
+    }
+
+    private fun re_authenticate() {
+        Log.v(TAG,"re-authenticate")
+        val user = FirebaseAuth.getInstance().currentUser
+            val authentication = EmailAuthProvider.getCredential(user?.email!!, getCurrentPasswordInput)
+
+        user.reauthenticate(authentication).addOnCompleteListener(object : OnCompleteListener<Void>{
+            override fun onComplete(task: Task<Void>) {
+                if(task.isSuccessful){
+                    user.updatePassword(new_confirm_password.text.toString()).addOnCompleteListener(object : OnCompleteListener<Void>{
+                        override fun onComplete(task: Task<Void>) {
+                            if(task.isSuccessful){
+                                Toast.makeText(this@changePassword, "Password Updated successfully", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this@changePassword, "Error password not updated. Try again sometime later", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    })
+
+                } else {
+                    Log.d(TAG, "Error authentication failed")
+                }
+            }
+
+        })
+    }
+
+
+    // To validate password
+    fun isValidPassword(password: String): Boolean {
+
+        val pattern: Pattern
+        val matcher: Matcher
+        val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$"
+        pattern = Pattern.compile(PASSWORD_PATTERN)
+        matcher = pattern.matcher(password)
+
+        return matcher.matches()
+
+    }
+
+    companion object {
+        fun getLaunchIntent(from: Context) = Intent(from, changePassword::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> startActivity(editProfileActivity.getLaunchIntent(this))
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/com/example/booksharingapp/editProfileActivity.kt
+++ app/src/main/java/com/example/booksharingapp/editProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.booksharingapp
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -9,6 +10,7 @@ import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import androidx.core.view.ViewCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
@@ -23,6 +25,7 @@ class editProfileActivity : AppCompatActivity() {
     private lateinit var currentUserID:String
     private lateinit var firstName:String
     private lateinit var lastName:String
+    private lateinit var password:String
     private lateinit var mUsersDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,8 +56,8 @@ class editProfileActivity : AppCompatActivity() {
                     val email = dataSnapshot.child("email").value.toString()
                     edit_profile_email_2.setText(email)
 
-                    val password = dataSnapshot.child("password").value.toString()
-                    edit_profile_password_2.setText(password)
+                    password = dataSnapshot.child("password").value.toString()
+
 
                    val profile_image = dataSnapshot.child("ProfileImage").value.toString()
                    Picasso.get()
@@ -63,99 +66,183 @@ class editProfileActivity : AppCompatActivity() {
                        .error(R.drawable.ic_profile_name)
                        .fit()
                        .into(edit_profile_image)
+
+                   if(! dataSnapshot.child("about_myself").exists()) {
+                       edit_profile_about_myself_2.setText(R.string.describe_yourself)
+                   } else {
+                       val about_myself = dataSnapshot.child("about_myself").value.toString()
+                       edit_profile_about_myself_2.setText(about_myself)
+                   }
                }
             }
 
         })
 
+
         edit_profile_username_button.setOnClickListener {
+            addViewsToEditUsername()
+        }
 
-            edit_profile_username_2.visibility = View.INVISIBLE
+        edit_profile_about_myself_button.setOnClickListener {
+            addViewsToEditAboutMyself()
+        }
 
-            // Edit first name
-            val edit_id = 1
-            val edit_first_name = EditText(this)
-            edit_first_name.id = edit_id
-            edit_first_name.setHint(firstName)
-            edit_first_name.setBackgroundResource(R.drawable.receiver_msg_txt)
-            val layoutParams= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-            layoutParams.addRule(RelativeLayout.BELOW, edit_profile_username_1.id)
-            edit_first_name.setPadding(20, 40, 20, 20)
-            edit_profile_relative_layout.addView(edit_first_name,layoutParams)
-
-            // Edit last name
-            val edit_last_name_id = 2
-            val edit_last_name = EditText(this)
-            edit_last_name.id = edit_last_name_id
-            edit_last_name.setBackgroundResource(R.drawable.receiver_msg_txt)
-            edit_last_name.setHint(lastName)
-            val layoutParams1= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-            layoutParams1.addRule(RelativeLayout.BELOW, edit_first_name.id)
-            edit_last_name.setPadding(20, 40, 20, 20)
-            edit_profile_relative_layout.addView(edit_last_name,layoutParams1)
-
-            // Add save button
-            val save_button_id = 3
-            val save_button = Button(this)
-            save_button.id = save_button_id
-            save_button.setText(R.string.save_text)
-            val layoutParams2= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-            layoutParams2.addRule(RelativeLayout.BELOW, edit_last_name.id)
-            layoutParams2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, edit_last_name.id)
-            save_button.setPadding(20, 20, 20, 20)
-            edit_profile_relative_layout.addView(save_button,layoutParams2)
-
-            // Add Cancel button
-            val cancel_button = Button(this)
-            cancel_button.setText(R.string.cancel_text)
-            val layoutParams3= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-            layoutParams3.addRule(RelativeLayout.BELOW, edit_last_name.id)
-            layoutParams3.addRule(RelativeLayout.START_OF, save_button.id)
-            cancel_button.setPadding(20, 20, 20, 20)
-            edit_profile_relative_layout.addView(cancel_button,layoutParams3)
-
-
-
-            cancel_button.setOnClickListener {
-                edit_profile_username_2.visibility = View.VISIBLE
-                edit_profile_relative_layout.removeView(save_button)
-                edit_profile_relative_layout.removeView(edit_first_name)
-                edit_profile_relative_layout.removeView(edit_last_name)
-                edit_profile_relative_layout.removeView(cancel_button)
-            }
-
-
-            save_button.setOnClickListener {
-                if(TextUtils.isEmpty(edit_first_name.text.toString()) || TextUtils.isEmpty(edit_last_name.text.toString())){
-                    Toast.makeText(this,"User name cannot be blank", Toast.LENGTH_SHORT).show()
-                } else {
-
-                    val edited_firstName = edit_first_name.text.toString()
-                    val edited_lastName = edit_last_name.text.toString()
-                    val userinfo_updates_hashMap = HashMap<String,Any>()
-                    userinfo_updates_hashMap.put("firstName", edited_firstName)
-                    userinfo_updates_hashMap.put("lastName", edited_lastName)
-
-                    val edited_username = edited_firstName + edited_lastName
-                    mUsersDbRef.updateChildren(userinfo_updates_hashMap)
-                        .addOnSuccessListener {
-                            edit_profile_username_2.setText(edited_username)
-                            edit_profile_username_2.visibility = View.VISIBLE
-                            edit_profile_relative_layout.removeView(save_button)
-                            edit_profile_relative_layout.removeView(edit_first_name)
-                            edit_profile_relative_layout.removeView(edit_last_name)
-                            edit_profile_relative_layout.removeView(cancel_button)
-                            edit_profile_relative_layout.removeView(save_button)
-                        }
-                        .addOnFailureListener {
-                            Log.v(TAG, it.printStackTrace().toString())
-                        }
-                }
-            }
-
+        edit_profile_password_button.setOnClickListener {
+            val startChangePassIntent = Intent(this, changePassword::class.java)
+            startChangePassIntent.putExtra("password", password)
+            startActivity(startChangePassIntent)
         }
     }
 
+    private fun addViewsToEditAboutMyself() {
+
+        edit_profile_about_myself_2.visibility = View.INVISIBLE
+
+        val edit_about_myself = EditText(this)
+        edit_about_myself.id = ViewCompat.generateViewId()
+        edit_about_myself.setBackgroundResource(R.drawable.receiver_msg_txt)
+        edit_about_myself.setHint(" ")
+        val layoutParams1= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams1.addRule(RelativeLayout.BELOW, edit_profile_about_myself_2.id)
+        layoutParams1.addRule(RelativeLayout.ALIGN_LEFT, edit_profile_about_myself_2.id)
+        edit_about_myself.setPadding(20, 40, 20, 20)
+        edit_profile_myself_layout.addView(edit_about_myself,layoutParams1)
+
+
+        // Dynamically created a SAVE button to save the user's About myself value.
+        val save_button = Button(this)
+        save_button.id = ViewCompat.generateViewId()
+        save_button.setText(R.string.save_text)
+        val layoutParams2= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams2.addRule(RelativeLayout.BELOW, edit_about_myself.id)
+        layoutParams2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, edit_about_myself.id)
+        save_button.setPadding(20, 20, 20, 20)
+        edit_profile_myself_layout.addView(save_button,layoutParams2)
+
+        //CANCEL button to cancel the dynamically created views
+        val cancel_button = Button(this)
+        cancel_button.setText(R.string.cancel_text)
+        val layoutParams3= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams3.addRule(RelativeLayout.BELOW, edit_about_myself.id)
+        layoutParams3.addRule(RelativeLayout.START_OF, save_button.id)
+        cancel_button.setPadding(20, 20, 20, 20)
+        edit_profile_myself_layout.addView(cancel_button,layoutParams3)
+
+        cancel_button.setOnClickListener {
+            edit_profile_about_myself_2.visibility = View.VISIBLE
+            edit_profile_myself_layout.removeView(save_button)
+            edit_profile_myself_layout.removeView(edit_about_myself)
+            edit_profile_myself_layout.removeView(cancel_button)
+        }
+
+        // SAVE the edited About_myself value in Firebase Database.
+        save_button.setOnClickListener {
+
+            val about_myself = edit_about_myself.text.toString()
+            if(TextUtils.isEmpty(about_myself)){
+                Toast.makeText(this,"About Myself field cannot be saved as blank", Toast.LENGTH_SHORT).show()
+            } else {
+                val userinfo_updates_hashMap = HashMap<String,Any>()
+                userinfo_updates_hashMap.put("about_myself", about_myself)
+
+                mUsersDbRef.updateChildren(userinfo_updates_hashMap)
+                    .addOnSuccessListener {
+                        edit_profile_about_myself_2.setText(about_myself)
+                        edit_profile_about_myself_2.visibility = View.VISIBLE
+                        edit_profile_myself_layout.removeView(save_button)
+                        edit_profile_myself_layout.removeView(edit_about_myself)
+                        edit_profile_myself_layout.removeView(cancel_button)
+                    }
+                    .addOnFailureListener {
+                        Log.v(TAG, it.printStackTrace().toString())
+                    }
+            }
+        }
+    }
+
+
+    // Added multiple views using Dynamic layout to edit Username
+    fun addViewsToEditUsername(){
+        Log.v("editProfile", "username-2")
+        edit_profile_username_2.visibility = View.INVISIBLE
+
+        // Edit first name
+        val edit_first_name = EditText(this)
+        edit_first_name.id = ViewCompat.generateViewId()
+        edit_first_name.setHint(firstName)
+        edit_first_name.setBackgroundResource(R.drawable.receiver_msg_txt)
+        val layoutParams= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams.addRule(RelativeLayout.BELOW, edit_profile_username_1.id)
+        edit_first_name.setPadding(20, 40, 20, 20)
+        edit_profile_relative_layout.addView(edit_first_name,layoutParams)
+
+        // Edit last name
+        val edit_last_name = EditText(this)
+        edit_last_name.id = ViewCompat.generateViewId()
+        edit_last_name.setBackgroundResource(R.drawable.receiver_msg_txt)
+        edit_last_name.setHint(lastName)
+        val layoutParams1= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams1.addRule(RelativeLayout.BELOW, edit_first_name.id)
+        edit_last_name.setPadding(20, 40, 20, 20)
+        edit_profile_relative_layout.addView(edit_last_name,layoutParams1)
+
+        // Add save button
+        val save_button = Button(this)
+        save_button.id = ViewCompat.generateViewId()
+        save_button.setText(R.string.save_text)
+        val layoutParams2= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams2.addRule(RelativeLayout.BELOW, edit_last_name.id)
+        layoutParams2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, edit_last_name.id)
+        save_button.setPadding(20, 20, 20, 20)
+        edit_profile_relative_layout.addView(save_button,layoutParams2)
+
+        // Add Cancel button
+        val cancel_button = Button(this)
+        cancel_button.setText(R.string.cancel_text)
+        val layoutParams3= RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams3.addRule(RelativeLayout.BELOW, edit_last_name.id)
+        layoutParams3.addRule(RelativeLayout.START_OF, save_button.id)
+        cancel_button.setPadding(20, 20, 20, 20)
+        edit_profile_relative_layout.addView(cancel_button,layoutParams3)
+
+        cancel_button.setOnClickListener {
+            edit_profile_username_2.visibility = View.VISIBLE
+            edit_profile_relative_layout.removeView(save_button)
+            edit_profile_relative_layout.removeView(edit_first_name)
+            edit_profile_relative_layout.removeView(edit_last_name)
+            edit_profile_relative_layout.removeView(cancel_button)
+        }
+
+
+        // SAVE the edited Username value in Firebase Database.
+        save_button.setOnClickListener {
+            if(TextUtils.isEmpty(edit_first_name.text.toString()) || TextUtils.isEmpty(edit_last_name.text.toString())){
+                Toast.makeText(this,"User name cannot be blank", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val edited_firstName = edit_first_name.text.toString()
+                val edited_lastName = edit_last_name.text.toString()
+                val userinfo_updates_hashMap = HashMap<String,Any>()
+                userinfo_updates_hashMap.put("firstName", edited_firstName)
+                userinfo_updates_hashMap.put("lastName", edited_lastName)
+
+                val edited_username = edited_firstName + edited_lastName
+                mUsersDbRef.updateChildren(userinfo_updates_hashMap)
+                    .addOnSuccessListener {
+                        edit_profile_username_2.setText(edited_username)
+                        edit_profile_username_2.visibility = View.VISIBLE
+                        edit_profile_relative_layout.removeView(save_button)
+                        edit_profile_relative_layout.removeView(edit_first_name)
+                        edit_profile_relative_layout.removeView(edit_last_name)
+                        edit_profile_relative_layout.removeView(cancel_button)
+                    }
+                    .addOnFailureListener {
+                        Log.v(TAG, it.printStackTrace().toString())
+                    }
+            }
+        }
+
+    }
     companion object {
         fun getLaunchIntent(from: Context) = Intent(from, editProfileActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
